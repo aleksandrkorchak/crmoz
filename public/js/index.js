73b718181900b0a3698f76036b01b8f4adc452f6
alexandr <korchak.alexandr@gmail.com>
2023-06-14 22:27:48 +0300

    chore: show access or error message for account and deal

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,18 @@ document.addEventListener("DOMNodeInserted", function (event) {
         el.classList.remove('bg-gray-900')
     }
 });
+
+//Show error or access message for 5 seconds
+document.addEventListener('DOMContentLoaded', function () {
+    const success = document.getElementById('message_success')
+    const error = document.getElementById('message_error')
+    if (success != null) {
+        setTimeout(function () {
+            success.remove()
+        }, 5000);
+    } else if (error != null) {
+        setTimeout(function () {
+            error.remove()
+        }, 5000);
+    }
+})
